2ea24d14d80b59ecf726dab3d8d968efacd3392f
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2023-08-25 18:50:52 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 set -e
 
 : "${ENDPOINT?Need to set ENDPOINT}"
-: "${SCHAIN_PROXY_DOMAIN?Need to set SCHAIN_PROXY_DOMAIN}"
+: "${PROXY_DOMAIN?Need to set SCHAIN_PROXY_DOMAIN}"
 
 export FLASK_APP_HOST=0.0.0.0
 export FLASK_APP_PORT=5000
-export FLASK_HOST_PORT=3009
+export FLASK_HOST_PORT=${API_PORT:-3009}
 
 docker-compose up -d --build
